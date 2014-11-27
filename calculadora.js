98f4895b520adfd55d97af4565940238d8ccb956
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2014-11-26 20:31:34 -0500

    primer intento de  realizar operaciones
    la operacion de barrido no funciona.

--- calculadora.js
+++ calculadora.js
@@ -3,15 +3,30 @@ var myApp=angular.module('myApp',[]);
 
 myApp.controller('control',function($scope){
 	var acumulador='';
+	var ac = 0;
 	$scope.valBtn=function(m){
 		acumulador=acumulador + m;
 		$scope.preResultado=acumulador;
+		ac = acumulador;
+		/*console.log(ac es la cadena acumulada);*/
+		console.log('cadena acumulada ' + ac);
+		console.log('longitud acumulada ' + ac.length);
+		/*$scope.prueba=typeof (ac/2);*/
+
 	};
 	$scope.reset=function(){
 		$scope.preResultado='';
-		acumulador='';
+		acumulador=' ';
 
 	};
 
-});
+	$scope.barrido=function(){
+		var contador=0;
 
+		for (var i =0; i< ac.length; i++) {
+			contador=i++
+
+		};
+		$scope.prueba= contador;
+	};
+})
